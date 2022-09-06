8e10f815a8e7a07ff11606290c4de9c98059439a
mitchellnel <mitchellmclin@gmail.com>
2022-09-06 17:30:10 +0800

    Create useAudioPlayer custom hook

---
+++ src/hooks/useAudioplayer.tsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react";
+
+function useAudioPlayer() {
+  const [playing, setPlayingFlag] = useState<boolean>();
+  const [duration, setDuration] = useState<number>();
+  const [currentTime, setCurrentTime] = useState<number>();
+  const [clickedTime, setClickedTime] = useState<number>();
+
+  useEffect(() => {
+    const audio_optional = document.querySelector<HTMLAudioElement>("audio");
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const audio: HTMLAudioElement = audio_optional!;
+
+    // state setter wrappers
+    const setAudioData = () => {
+      setDuration(audio.duration);
+      setCurrentTime(audio.currentTime);
+    };
+
+    const setAudioTime = () => setCurrentTime(audio.currentTime);
+
+    // DOM listeners -- update React state on DOM events
+    audio.addEventListener("loadeddata", setAudioData);
+    audio.addEventListener("timeUpdate", setAudioTime);
+
+    // React state listeners -- update DOM on React state changes
+    playing ? audio.play() : audio.pause();
+
+    if (clickedTime && clickedTime !== currentTime) {
+      audio.currentTime = clickedTime;
+      setClickedTime(0);
+    }
+
+    // effect cleanup
+    return () => {
+      audio.removeEventListener("loadeddata", setAudioData);
+      audio.removeEventListener("timeUpdate", setAudioTime);
+    };
+  }, [playing, clickedTime, currentTime]);
+
+  return {
+    playing,
+    duration,
+    currentTime,
+    clickedTime,
+    setPlayingFlag,
+    setClickedTime,
+  };
+}
+
+export default useAudioPlayer;
+
+//     // React state listeners: update DOM on React state changes
+//     playing ? audio.play() : audio.pause();
+
+//     if (clickedTime && clickedTime !== curTime) {
+//       audio.currentTime = clickedTime;
+//       setClickedTime(null);
+//     }
+
+//     // effect cleanup
+//     return () => {
+//       audio.removeEventListener("loadeddata", setAudioData);
+//       audio.removeEventListener("timeupdate", setAudioTime);
+//     };
+//   });
+
+//   return {
+//     curTime,
+//     duration,
+//     playing,
+//     setPlaying,
+//     setClickedTime,
+//   };
+// }
+
+// export default useAudioPlayer;
