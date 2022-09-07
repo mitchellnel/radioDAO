6ce2fb0e9dbfc5d473104bf8e14dd9507d37d444
mitchellnel <mitchellmclin@gmail.com>
2022-09-07 20:11:19 +0800

    Implement PlayerDetails component

--- src/components/Main/PlayerDetails/PlayerDetails.tsx
+++ src/components/Main/PlayerDetails/PlayerDetails.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./PlayerDetails.css";
 
-function PlayerDetails() {
-  return <div>PlayerDetails</div>;
+interface PlayerDetailsProps {
+  songTitle: string;
+  artist: string;
+}
+
+function PlayerDetails({ songTitle, artist }: PlayerDetailsProps) {
+  return (
+    <div>
+      <h1 id="song-title">{songTitle}</h1>
+
+      {/* TODO: get a lower weight Outfit font for h2 */}
+      <h2 id="artist">{artist}</h2>
+    </div>
+  );
 }
 
 export default PlayerDetails;
