389e978398da26ee56404d007324aa4687284c48
mitchellnel <mitchellmclin@gmail.com>
2022-09-23 13:54:50 -0700

    Create RDIONFT contract

--- contracts/RadioDAONFT.sol
+++ contracts/RadioDAONFT.sol
@@ -1,6 +1,70 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract RadioDAONFT {
-    
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+error AlreadyInitialised();
+error RangeOutOfBounds();
+
+contract RadioDAONFT is ERC721URIStorage, Ownable {
+    // NFT Contract Variables
+    bool private s_isInitialised;
+
+    uint256 private s_tokenCounter;
+    string[] internal s_tokenURIs;
+
+    // Contract Events
+    event NFTMinted(address minter);
+    event NFTTokenURISet(uint256 indexed tokenID);
+
+    constructor(string[16] memory tokenURIs) ERC721("RadioDAONFT", "RDIONFT") {
+        _initialiseContract(tokenURIs);
+    }
+
+    // Storage Variable Getters //
+    function getInitialisedFlag() public view returns (bool) {
+        return s_isInitialised;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+
+    function getTokenURI(uint256 index) public view returns (string memory) {
+        if (index < 0 || index > 15) {
+            revert RangeOutOfBounds();
+        }
+
+        return s_tokenURIs[index];
+    }
+
+    //
+
+    // Constructore Helper //
+    function _initialiseContract(string[16] memory tokenURIs) private {
+        if (s_isInitialised) {
+            revert AlreadyInitialised();
+        }
+
+        s_tokenURIs = tokenURIs;
+        s_isInitialised = true;
+    }
+
+    //
+
+    // Minting //
+    function mintNFT() public returns (uint256 tokenID) {
+        address minter = msg.sender;
+        uint256 newTokenID = s_tokenCounter;
+
+        _safeMint(minter, newTokenID);
+        emit NFTMinted(minter);
+
+        _setTokenURI(newTokenID, s_tokenURIs[newTokenID]);
+        emit NFTTokenURISet(newTokenID);
+
+        return newTokenID;
+    }
+    //
 }
