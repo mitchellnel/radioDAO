46e805e5babc7bc0b116ec8c7741b54aeaef96d2
mitchellnel <mitchellmclin@gmail.com>
2022-10-03 15:54:53 -0700

    Create hook to retrieve tokenURIs

---
+++ src/hooks/radioDAONFT/useTokenURI.tsx
@@ -0,0 +1,26 @@
+import { useCall } from "@usedapp/core";
+import { Contract, utils } from "ethers";
+
+function useTokenURI(
+  nftABI: any,
+  nftAddress: string,
+  tokenID: number
+): string | undefined {
+  const nftInterface = new utils.Interface(nftABI);
+  const nftContract = new Contract(nftAddress, nftInterface);
+
+  const { value, error } =
+    useCall({
+      contract: nftContract,
+      method: "tokenURI",
+      args: [tokenID],
+    }) ?? {};
+
+  if (error) {
+    return undefined;
+  }
+
+  return value;
+}
+
+export { useTokenURI };
