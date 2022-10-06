cf028ad62528387bdb768151871d8504c8be3fae
mitchellnel <mitchellmclin@gmail.com>
2022-10-05 21:49:01 -0700

    Add and stylise information in notification modal

--- src/components/shared/NotificationModal/NotificationModal.tsx
+++ src/components/shared/NotificationModal/NotificationModal.tsx
@@ -10,7 +10,7 @@ const modalBoxStyle = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 800,
+  width: 900,
   bgcolor: "primary.light",
   border: "2px solid #e8bd30",
   boxShadow: 24,
@@ -24,19 +24,35 @@ interface NotificationModalProps {
   successNotification: SuccessNotification;
 }
 
-function ListingModal({
+function NotificationModal({
   isVisible,
   onClose,
   successNotification,
 }: NotificationModalProps) {
   const [txnHash, setTxnHash] = useState<string>("");
   const [txnName, setTxnName] = useState<string>("");
+
   useEffect(() => {
     if (successNotification !== undefined) {
       setTxnHash(successNotification.receipt.transactionHash);
       setTxnName(successNotification.transactionName as string);
     }
   }, [successNotification]);
+
+  const [displayMsg, setDisplayMsg] = useState<string>("");
+
+  useEffect(() => {
+    if (txnName === "Buy NFT") {
+      setDisplayMsg("You have successfully purchased a RadioDAO NFT!");
+    } else if (txnName === "Sell NFT") {
+      setDisplayMsg("You have successfully listed your RadioDAO NFT for sale!");
+    } else if (txnName === "Delist NFT") {
+      setDisplayMsg(
+        "You have successfully delisted your RadioDAO NFT from the marketplace!"
+      );
+    }
+  }, [txnName]);
+
   return (
     <Modal
       open={isVisible}
@@ -49,9 +65,9 @@ function ListingModal({
           className="flex flex-row items-center"
           style={{ justifyContent: "space-between" }}
         >
-          <div className="flex flex-col">
+          <div className="flex flex-col gap-y-2">
             <Typography
-              id="modal-listing-modal"
+              id="modal-notification-modal-success"
               variant="h2"
               component="h2"
               color="primary.contrastText"
@@ -60,10 +76,29 @@ function ListingModal({
             >
               Success!
             </Typography>
-            <p>{txnHash}</p>
-            <p>{txnName}</p>
+            <Typography
+              id="modal-notification-modal-display-msg"
+              variant="h6"
+              component="h6"
+              color="primary.contrastText"
+              fontFamily="Outfit"
+              fontWeight="600"
+            >
+              {displayMsg}
+            </Typography>
+            <Typography
+              id="modal-notification-modal-display-txn-hash"
+              variant="h6"
+              component="h6"
+              color="primary.contrastText"
+              fontFamily="Outfit"
+              fontWeight="600"
+            >
+              Your transaction hash is: {txnHash}
+            </Typography>
           </div>
           <IconButton
+            className="self-start"
             aria-label="close-modal"
             color="secondary"
             onClick={onClose}
@@ -86,4 +121,4 @@ function ListingModal({
   );
 }
 
-export default ListingModal;
+export default NotificationModal;
