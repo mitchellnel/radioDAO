b5f4c5701aede535dd3aa283b100f1233bd15413
mitchellnel <mitchellmclin@gmail.com>
2022-10-06 02:21:51 -0700

    Create governance-related contracts

---
+++ contracts/RadioTimeLock.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract RadioTimeLock is TimelockController {
+    // minDelay - how long we have to wait before executing == 1 block ~ 12s
+    constructor(
+        address[] memory proposers,
+        address[] memory executors,
+        address admin
+    ) TimelockController(12, proposers, executors, admin) {}
+}
