c685acf1b1630a53deaf2b23f0688f7489032ed1
mitchellnel <mitchellmclin@gmail.com>
2022-10-06 05:05:44 -0700

    Split NFT marketplace functionality into its own contract

--- contracts/RadioDAONFT.sol
+++ contracts/RadioDAONFT.sol
@@ -1,23 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
 
 error AlreadyInitialised();
 
-contract RadioDAONFT is
-    ERC721Enumerable,
-    ERC721URIStorage,
-    ERC721Votes,
-    Ownable
-{
-    // NEL Variables
-    IERC20 public NEL_CONTRACT;
-
+contract RadioDAONFT is ERC721Enumerable, ERC721URIStorage, ERC721Votes {
     // NFT Variables
     bool private s_isInitialised;
 
@@ -25,60 +15,26 @@ contract RadioDAONFT is
     uint256 private s_tokenCounter;
     string[] internal s_tokenURIs;
 
-    // Marketplace Variables
-    struct MarketItem {
-        uint256 tokenID;
-        address payable seller;
-        uint256 price;
-        bool forSale;
-    }
-
-    MarketItem[] public s_marketItems;
-    uint256 public s_marketplaceFee; // in NEL
-
     // NFT Events
     event NFTMinted(address minter, uint256 indexed tokenID);
     event NFTTokenURISet(uint256 indexed tokenID, string tokenURI);
 
-    // Marketplace Events
-    event MarketItemBought(
-        uint256 indexed tokenID,
-        address indexed seller,
-        address indexed buyer,
-        uint256 price
-    );
-    event MarketItemListed(
-        uint256 indexed tokenID,
-        address indexed seller,
-        uint256 price
-    );
-    event MarketItemDelisted(uint256 indexed tokenID, address indexed seller);
-
-    constructor(
-        address nelContract,
-        string[MAX_TOKENS] memory tokenURIs,
-        uint256 marketplaceFee
-    ) ERC721("RadioDAONFT", "RDIO") EIP712("RadioDAONFT", "1") {
-        _initialiseContract(nelContract, tokenURIs, marketplaceFee);
+    constructor(string[MAX_TOKENS] memory tokenURIs)
+        ERC721("RadioDAONFT", "RDIO")
+        EIP712("RadioDAONFT", "1")
+    {
+        _initialiseContract(tokenURIs);
     }
 
     // Constructor Helpers //
-    function _initialiseContract(
-        address nelContract,
-        string[MAX_TOKENS] memory tokenURIs,
-        uint256 marketplaceFee
-    ) private {
+    function _initialiseContract(string[MAX_TOKENS] memory tokenURIs) private {
         if (s_isInitialised) {
             revert AlreadyInitialised();
         }
 
-        NEL_CONTRACT = IERC20(nelContract);
-
         s_tokenCounter = 0;
         s_tokenURIs = tokenURIs;
         s_isInitialised = true;
-
-        s_marketplaceFee = marketplaceFee;
 
         _mintAllNFTs();
     }
@@ -97,28 +53,12 @@ contract RadioDAONFT is
             string memory newTokenURI = s_tokenURIs[newTokenID];
             _setTokenURI(newTokenID, newTokenURI);
             emit NFTTokenURISet(newTokenID, newTokenURI);
-
-            // after an NFT is minted, list it for sale
-            MarketItem memory newItem;
-            newItem.tokenID = newTokenID;
-            newItem.seller = payable(minter);
-            newItem.price = 1_000_000_000_000_000_000; // 1 NEL = 10^18 NELwei
-            newItem.forSale = true;
-
-            s_marketItems.push(newItem);
         }
     }
 
     //
 
-    // NEL Contract Address Updater
-    function _updateNELContractAddress(address newAddress) external onlyOwner {
-        NEL_CONTRACT = IERC20(newAddress);
-    }
-
-    //
-
-    // Overrided functions to support ERC721Enumerable, ERC721URIStorage, and ERC721Votes extensions (as required by Solidity)
+    // Overrided functions to support ERC721Enumerable, ERC721URIStorage, and ERC721Votes extensions (as required by Solidity) //
     function tokenURI(uint256 tokenId)
         public
         view
@@ -201,19 +141,6 @@ contract RadioDAONFT is
 
     //
 
-    // Marketplace Variable Getters
-    function getMarketplaceFee() public view returns (uint256) {
-        return s_marketplaceFee;
-    }
-
-    //
-
-    // Minting //
-    // only allower owner of the contract to mint -- we'll pre-mint all the
-    //  NFTs and then list for sale on the marketplace
-
-    //
-
     // User Collection Functions //
     function getUserNFTs(address user)
         external
@@ -230,97 +157,5 @@ contract RadioDAONFT is
         return ownedNFTs;
     }
 
-    //
-
-    // Marketplace Functions //
-    function updateMarketplaceFee(uint256 newMarketplaceFee)
-        external
-        onlyOwner
-    {
-        s_marketplaceFee = newMarketplaceFee;
-    }
-
-    function buyNFT(uint256 tokenID) external payable {
-        uint256 buyPrice = s_marketItems[tokenID].price;
-        address seller = s_marketItems[tokenID].seller;
-
-        // validation checks
-        require(
-            msg.sender != seller,
-            "You cannot purchase the NFT that you already owned and have listed. Instead, delist the NFT from the marketplace."
-        );
-        // require(
-        //     msg.value == buyPrice,
-        //     "You either sent too little or too much ETH. Please send the asking price to complete the transaction."
-        // );
-        require(
-            s_marketItems[tokenID].forSale,
-            "This item is not for sale. How did you manage to try and purchase it?"
-        );
-
-        // use the bool in the MarketItem to effectively delist the item from sale
-        s_marketItems[tokenID].forSale = false;
-
-        // front-end will approve spending of buyPrice NEL tokens
-
-        // complete the purchase transaction
-        _transfer(address(this), msg.sender, tokenID);
-        NEL_CONTRACT.transferFrom(msg.sender, seller, buyPrice);
-        emit MarketItemBought(tokenID, seller, msg.sender, buyPrice);
-    }
-
-    function sellNFT(uint256 tokenID, uint256 salePrice) external payable {
-        // require(
-        //     msg.value == s_marketplaceFee,
-        //     "A fee must be paid to the marketplace to list your NFT. This fee must be exactly the marketplace fee."
-        // );
-        require(
-            salePrice > 0,
-            "You cannot list your NFT for a price less than zero. Please set a price greater than zero."
-        );
-
-        // list the item for sale, with updated sale parameters
-        s_marketItems[tokenID].price = salePrice;
-        s_marketItems[tokenID].seller = payable(msg.sender);
-        s_marketItems[tokenID].forSale = true;
-
-        // front-end will approve spending of s_marketplaceFee NEL tokens
-
-        // complete the listing transaction
-        NEL_CONTRACT.transferFrom(msg.sender, owner(), s_marketplaceFee);
-        _transfer(msg.sender, address(this), tokenID);
-        emit MarketItemListed(tokenID, msg.sender, salePrice);
-    }
-
-    function delistNFT(uint256 tokenID) external {
-        require(
-            msg.sender == s_marketItems[tokenID].seller,
-            "You cannot delist an NFT that you are not the seller of."
-        );
-        // use the bool in the MarketItem to effectively delist the item from sale
-        s_marketItems[tokenID].forSale = false;
-
-        // seller forfeits marketplace fee
-
-        // complete delisting transaction
-        _transfer(address(this), msg.sender, tokenID);
-        emit MarketItemDelisted(tokenID, msg.sender);
-    }
-
-    function getAllNFTsForSale() external view returns (MarketItem[] memory) {
-        uint256 numNFTsForSale = balanceOf(address(this));
-        MarketItem[] memory NFTsForSale = new MarketItem[](numNFTsForSale);
-
-        // look through s_marketItems and find those MarketItems for which forSale == true
-        uint256 currentIndex;
-        for (uint256 i = 0; i < s_marketItems.length; i++) {
-            if (s_marketItems[i].forSale) {
-                NFTsForSale[currentIndex] = s_marketItems[i];
-                currentIndex++;
-            }
-        }
-
-        return NFTsForSale;
-    }
     //
 }
