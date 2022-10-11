20da539b8658e4a729b407891c338d2790412f94
mitchellnel <mitchellmclin@gmail.com>
2022-10-11 12:33:35 -0700

    Add defeat test and execute from another account test

--- test/RadioDAO.test.ts
+++ test/RadioDAO.test.ts
@@ -1,15 +1,11 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { assert, expect } from "chai";
-import {
-  BigNumber,
-  BigNumberish,
-  constants,
-  Contract,
-  ContractFactory,
-} from "ethers";
+import { BigNumber, BigNumberish } from "ethers";
 import { deployments, ethers, network } from "hardhat";
 
 import {
+  Nelthereum,
+  RadioDAOMarketplace,
   RadioDAONFT,
   RadioDAO,
   RadioTimelock,
@@ -18,6 +14,10 @@ import {
 
 import { developmentChains } from "../helper-hardhat-config";
 import { moveBlocks, moveTime } from "../scripts/utils";
+
+const toWei = (num: Number) => ethers.utils.parseEther(num.toString());
+const fromWei = (num: BigNumberish) => ethers.utils.formatEther(num);
+
 // eslint-disable-next-line @typescript-eslint/no-unused-expressions
 !developmentChains.includes(network.name)
   ? describe.skip
@@ -26,7 +26,9 @@ import { moveBlocks, moveTime } from "../scripts/utils";
       const FOR_VOTE = 1;
       const ABSTAIN_VOTE = 2;
 
-      let deployer: SignerWithAddress;
+      let deployer: SignerWithAddress, user1: SignerWithAddress;
+      let nel: Nelthereum;
+      let rdioMarketplace: RadioDAOMarketplace;
       let rdioNFT: RadioDAONFT;
       let dao: RadioDAO;
       let timelock: RadioTimelock;
@@ -35,10 +37,12 @@ import { moveBlocks, moveTime } from "../scripts/utils";
       let votingDelay: BigNumber, votingPeriod: BigNumber, minDelay: BigNumber;
 
       beforeEach(async () => {
-        [deployer] = await ethers.getSigners();
+        [deployer, user1] = await ethers.getSigners();
 
         await deployments.fixture(["all"]);
 
+        nel = await ethers.getContract("Nelthereum");
+        rdioMarketplace = await ethers.getContract("RadioDAOMarketplace");
         rdioNFT = await ethers.getContract("RadioDAONFT");
         dao = await ethers.getContract("RadioDAO");
         timelock = await ethers.getContract("RadioTimelock");
@@ -47,6 +51,52 @@ import { moveBlocks, moveTime } from "../scripts/utils";
         votingDelay = await dao.votingDelay();
         votingPeriod = await dao.votingPeriod();
         minDelay = await timelock.getMinDelay();
+
+        await nel.connect(user1).requestTokens();
+      });
+
+      describe("Transfer of Voting Power", () => {
+        let originalSellerVotingPower: BigNumber,
+          originalBuyerVotingPower: BigNumber;
+        beforeEach(async () => {
+          await nel
+            .connect(deployer)
+            .approve(
+              rdioMarketplace.address,
+              await rdioMarketplace.getMarketplaceFee()
+            );
+          await nel.connect(user1).approve(rdioMarketplace.address, toWei(1));
+
+          await rdioNFT.connect(deployer).approve(rdioMarketplace.address, 3);
+
+          originalSellerVotingPower = await rdioNFT.getVotes(deployer.address);
+
+          originalBuyerVotingPower = await rdioNFT.getVotes(user1.address);
+
+          await rdioMarketplace.connect(deployer).sellNFT(3, toWei(1));
+        });
+
+        it("Should correctly transfer the voting power to a buyer on the marketplace", async () => {
+          const newSellerVotingPower = await rdioNFT.getVotes(deployer.address);
+
+          assert.equal(
+            originalSellerVotingPower.sub(newSellerVotingPower).toString(),
+            "1"
+          );
+
+          // user1 buys the NFT
+          await rdioMarketplace.connect(user1).buyNFT(3);
+
+          // user 1 registers to vote in the DAO
+          await rdioNFT.connect(user1).delegate(user1.address);
+
+          const newBuyerVotingPower = await rdioNFT.getVotes(user1.address);
+
+          assert.equal(
+            newBuyerVotingPower.sub(originalBuyerVotingPower).toString(),
+            "1"
+          );
+        });
       });
 
       describe("Radio Functionality", () => {
@@ -60,7 +110,7 @@ import { moveBlocks, moveTime } from "../scripts/utils";
       describe("Governance Workflow", () => {
         it("Should propose, vote FOR, wait, queue, and then execute", async () => {
           // propose
-          console.log("\nProposing ...");
+          console.log("\n\t\tProposing ...");
           const encodedFunctionCall = radio.interface.encodeFunctionData(
             "queueSong",
             ["song uri"]
@@ -83,15 +133,15 @@ import { moveBlocks, moveTime } from "../scripts/utils";
           assert.equal(proposalState.toString(), "0");
 
           console.log(
-            `Current state of Proposal ${proposalId} is: ${proposalState}`
+            `\t\tCurrent state of Proposal ${proposalId} is: ${proposalState}`
           );
 
           // wait
-          console.log("\n... Waiting ...\n");
+          console.log("\n\t\t... Waiting ...\n");
           await moveBlocks(votingDelay.add(1).toNumber());
 
           // vote
-          console.log("\nVoting ...");
+          console.log("\n\t\tVoting ...");
           const voteTxn = await dao.castVoteWithReason(
             proposalId,
             FOR_VOTE,
@@ -105,11 +155,11 @@ import { moveBlocks, moveTime } from "../scripts/utils";
           assert.equal(proposalState.toString(), "1");
 
           console.log(
-            `Current state of Proposal ${proposalId} is: ${proposalState}`
+            `\t\tCurrent state of Proposal ${proposalId} is: ${proposalState}`
           );
 
           // wait
-          console.log("\n... Waiting ...\n");
+          console.log("\n\t\t... Waiting ...\n");
           await moveBlocks(votingPeriod.add(10).toNumber());
 
           expect(await dao.hasVoted(proposalId, deployer.address)).to.be.true;
@@ -120,11 +170,11 @@ import { moveBlocks, moveTime } from "../scripts/utils";
           assert.equal(proposalState.toString(), "4");
 
           console.log(
-            `Current state of Proposal ${proposalId} is: ${proposalState}`
+            `\t\tCurrent state of Proposal ${proposalId} is: ${proposalState}`
           );
 
           // queue
-          console.log("\nQueueing ...");
+          console.log("\n\t\tQueueing ...");
           const descriptionHash = ethers.utils.keccak256(
             ethers.utils.toUtf8Bytes('Queue the song with URI "song uri"')
           );
@@ -138,7 +188,7 @@ import { moveBlocks, moveTime } from "../scripts/utils";
           await queueTxn.wait(1);
 
           // wait
-          console.log("\n... Waiting ...\n");
+          console.log("\n\t\t... Waiting ...\n");
           await moveTime(minDelay.add(1).toNumber());
           await moveBlocks(1);
 
@@ -148,11 +198,11 @@ import { moveBlocks, moveTime } from "../scripts/utils";
           assert.equal(proposalState.toString(), "5");
 
           console.log(
-            `Current state of Proposal ${proposalId} is: ${proposalState}`
+            `\t\tCurrent state of Proposal ${proposalId} is: ${proposalState}`
           );
 
           // execute
-          console.log("\nExecuting ...");
+          console.log("\n\t\tExecuting ...");
           const executeTxn = await dao.execute(
             [radio.address],
             [0],
@@ -167,7 +217,205 @@ import { moveBlocks, moveTime } from "../scripts/utils";
           assert.equal(proposalState.toString(), "7");
 
           console.log(
-            `Current state of Proposal ${proposalId} is: ${proposalState}`
+            `\t\tCurrent state of Proposal ${proposalId} is: ${proposalState}`
+          );
+
+          expect(await radio.getNextSong()).to.equal("song uri");
+        });
+
+        it("Should propose, vote AGAINST, and then be rejected", async () => {
+          // propose
+          console.log("\n\t\tProposing ...");
+          const encodedFunctionCall = radio.interface.encodeFunctionData(
+            "queueSong",
+            ["song uri"]
+          );
+
+          const proposeTxn = await dao
+            .connect(deployer)
+            .propose(
+              [radio.address],
+              [0],
+              [encodedFunctionCall],
+              'Queue the song with URI "song uri"'
+            );
+          const proposeReceipt = await proposeTxn.wait(1);
+
+          const proposalId = proposeReceipt.events![0].args!.proposalId;
+          let proposalState = await dao.state(proposalId);
+
+          // proposal state should be 0 == Pending
+          assert.equal(proposalState.toString(), "0");
+
+          console.log(
+            `\t\tCurrent state of Proposal ${proposalId} is: ${proposalState}`
+          );
+
+          // wait
+          console.log("\n\t\t... Waiting ...\n");
+          await moveBlocks(votingDelay.add(1).toNumber());
+
+          // vote
+          console.log("\n\t\tVoting ...");
+          const voteTxn = await dao.castVoteWithReason(
+            proposalId,
+            AGAINST_VOTE,
+            "This is my favourite song"
+          );
+          await voteTxn.wait(1);
+
+          proposalState = await dao.state(proposalId);
+
+          // proposal state should be 1 == Active
+          assert.equal(proposalState.toString(), "1");
+
+          console.log(
+            `\t\tCurrent state of Proposal ${proposalId} is: ${proposalState}`
+          );
+
+          // wait
+          console.log("\n\t\t... Waiting ...\n");
+          await moveBlocks(votingPeriod.add(10).toNumber());
+
+          expect(await dao.hasVoted(proposalId, deployer.address)).to.be.true;
+
+          proposalState = await dao.state(proposalId);
+
+          // proposal state should be 3 == Defeated
+          assert.equal(proposalState.toString(), "3");
+
+          console.log(
+            `\t\tCurrent state of Proposal ${proposalId} is: ${proposalState}`
+          );
+
+          // queue
+          console.log("\n\t\tAttempting to Queue ...");
+          const descriptionHash = ethers.utils.keccak256(
+            ethers.utils.toUtf8Bytes('Queue the song with URI "song uri"')
+          );
+
+          await expect(
+            dao.queue(
+              [radio.address],
+              [0],
+              [encodedFunctionCall],
+              descriptionHash
+            )
+          ).to.be.rejectedWith("Governor: proposal not successful");
+        });
+
+        it("Should allow anyone to execute a proposal", async () => {
+          // propose
+          console.log("\n\t\tProposing ...");
+          const encodedFunctionCall = radio.interface.encodeFunctionData(
+            "queueSong",
+            ["song uri"]
+          );
+
+          const proposeTxn = await dao
+            .connect(deployer)
+            .propose(
+              [radio.address],
+              [0],
+              [encodedFunctionCall],
+              'Queue the song with URI "song uri"'
+            );
+          const proposeReceipt = await proposeTxn.wait(1);
+
+          const proposalId = proposeReceipt.events![0].args!.proposalId;
+          let proposalState = await dao.state(proposalId);
+
+          // proposal state should be 0 == Pending
+          assert.equal(proposalState.toString(), "0");
+
+          console.log(
+            `\t\tCurrent state of Proposal ${proposalId} is: ${proposalState}`
+          );
+
+          // wait
+          console.log("\n\t\t... Waiting ...\n");
+          await moveBlocks(votingDelay.add(1).toNumber());
+
+          // vote
+          console.log("\n\t\tVoting ...");
+          const voteTxn = await dao.castVoteWithReason(
+            proposalId,
+            FOR_VOTE,
+            "This is my favourite song"
+          );
+          await voteTxn.wait(1);
+
+          proposalState = await dao.state(proposalId);
+
+          // proposal state should be 1 == Active
+          assert.equal(proposalState.toString(), "1");
+
+          console.log(
+            `\t\tCurrent state of Proposal ${proposalId} is: ${proposalState}`
+          );
+
+          // wait
+          console.log("\n\t\t... Waiting ...\n");
+          await moveBlocks(votingPeriod.add(10).toNumber());
+
+          expect(await dao.hasVoted(proposalId, deployer.address)).to.be.true;
+
+          proposalState = await dao.state(proposalId);
+
+          // proposal state should be 4 == Succeeded
+          assert.equal(proposalState.toString(), "4");
+
+          console.log(
+            `\t\tCurrent state of Proposal ${proposalId} is: ${proposalState}`
+          );
+
+          // queue
+          console.log("\n\t\tQueueing ...");
+          const descriptionHash = ethers.utils.keccak256(
+            ethers.utils.toUtf8Bytes('Queue the song with URI "song uri"')
+          );
+
+          const queueTxn = await dao.queue(
+            [radio.address],
+            [0],
+            [encodedFunctionCall],
+            descriptionHash
+          );
+          await queueTxn.wait(1);
+
+          // wait
+          console.log("\n\t\t... Waiting ...\n");
+          await moveTime(minDelay.add(1).toNumber());
+          await moveBlocks(1);
+
+          proposalState = await dao.state(proposalId);
+
+          // proposal state should be 5 == Queued
+          assert.equal(proposalState.toString(), "5");
+
+          console.log(
+            `\t\tCurrent state of Proposal ${proposalId} is: ${proposalState}`
+          );
+
+          // execute
+          console.log("\n\t\tExecuting from another account...");
+          const executeTxn = await dao
+            .connect(user1)
+            .execute(
+              [radio.address],
+              [0],
+              [encodedFunctionCall],
+              descriptionHash
+            );
+          await executeTxn.wait(1);
+
+          proposalState = await dao.state(proposalId);
+
+          // proposal state should be 7 == Executed
+          assert.equal(proposalState.toString(), "7");
+
+          console.log(
+            `\t\tCurrent state of Proposal ${proposalId} is: ${proposalState}`
           );
 
           expect(await radio.getNextSong()).to.equal("song uri");
