82bcf13c2038cc3f9c30dae39bbb56449533b1dc
mitchellnel <mitchellmclin@gmail.com>
2022-10-13 01:26:21 -0700

    Create hook to get delegate of user

--- src/hooks/radioDAONFT/index.ts
+++ src/hooks/radioDAONFT/index.ts
@@ -1,3 +1,4 @@
 export { useTokenURI } from "./useTokenURI";
 export { useGetMyNFTs } from "./useGetMyNFTs";
 export { useGetVotes } from "./useGetVotes";
+export { useGetDelegate } from "./useGetDelegate";
