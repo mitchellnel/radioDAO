c0c82eb1f4ad74f0b9f79859d14825f0efce8ceb
mitchellnel <mitchellmclin@gmail.com>
2022-10-15 01:17:05 -0700

    Create colours for vote text and buttons

--- src/assets/RadioDAOTheme.tsx
+++ src/assets/RadioDAOTheme.tsx
@@ -1,4 +1,27 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme, PaletteColorOptions } from "@mui/material/styles";
+
+declare module "@mui/material/styles" {
+  interface CustomPalette {
+    voteFor: PaletteColorOptions;
+    voteAgainst: PaletteColorOptions;
+    voteAbstain: PaletteColorOptions;
+  }
+  interface Palette extends CustomPalette {}
+  interface PaletteOptions extends CustomPalette {}
+}
+
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    voteFor: true;
+    voteAgainst: true;
+    voteAbstain: true;
+  }
+}
+
+const { palette } = createTheme();
+const { augmentColor } = palette;
+const createColor = (mainColor: string) =>
+  augmentColor({ color: { main: mainColor } });
 
 const radioDAOTheme = createTheme({
   palette: {
@@ -10,6 +33,9 @@ const radioDAOTheme = createTheme({
     secondary: {
       main: "#e8bd30",
     },
+    voteFor: createColor("#3f7a32"),
+    voteAgainst: createColor("#df3633"),
+    voteAbstain: createColor("#9867c5"),
   },
 });
 
