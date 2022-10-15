23e0f2477113d1d13ae4212447584a640ba6cd1f
mitchellnel <mitchellmclin@gmail.com>
2022-10-14 19:03:56 -0700

    Create hook to propose a song to the RadioDAO

--- src/hooks/radioDAO/index.ts
+++ src/hooks/radioDAO/index.ts
@@ -1,2 +1,3 @@
 export { useProposalState } from "./useProposalState";
 export { useGetAllProposalInformation } from "./useGetAllProposalInformation";
+export { useProposeSong } from "./useProposeSong";
